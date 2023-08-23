b8578df570fff10f2b6c673e40735c576673bedd
besufikad17 <besufikadtilahun1994@gmail.com>
2023-08-24 01:42:36 +0300

    location picker implemented

--- lib/components/location_picker.dart
+++ lib/components/location_picker.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:waliyalift/components/text.dart';
 import 'package:waliyalift/models/place.dart';
+import 'package:waliyalift/utils/color.dart';
 import 'package:waliyalift/utils/data.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class LocationPicker extends StatelessWidget {
+class LocationPicker extends HookConsumerWidget {
   LocationPicker({
     super.key, 
     required this.places, 
@@ -17,55 +21,71 @@ class LocationPicker extends StatelessWidget {
   TextEditingController search = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
-    List<String> cities = getFullAdress(places);
-    return Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(15),
-            child: TextField(  
-              controller: search,  
-              decoration: const InputDecoration(  
-                border: OutlineInputBorder(),  
-                labelText: 'Place',  
-                hintText: 'Search place..',  
-              ), 
-              onChanged: (query) {
-                List<String> result = [];
-                if(query.isEmpty){
-                  cities = getFullAdress(places);
-                }else {
-                  for(var place in cities) {
-                    if(place.toLowerCase().contains(query.toLowerCase())){
-                      result.add(place);
+  Widget build(BuildContext context, WidgetRef ref) {
+    var cityState = useState(getFullAdress(places));
+    List<String> cities = cityState.value;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: MyText(
+            text: "$flag location",
+            size: 12,
+            bgcolor: colorToHexString(Theme.of(context).colorScheme.primary),
+            fgcolor: "#ffffff",
+            isBold: true,
+            borderRadius: 0),
+        centerTitle: true,
+      ),
+      body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(  
+                controller: search,  
+                decoration: const InputDecoration(  
+                  border: OutlineInputBorder(),  
+                  labelText: 'Place',  
+                  hintText: 'Search place..',  
+                ), 
+                onChanged: (query) {
+                  List<String> result = [];
+                  if(query.isEmpty){ 
+                    cityState.value = getFullAdress(places);
+                  }else {
+                    for(var place in cities) {
+                      if(place.toLowerCase().contains(query.toLowerCase())){
+                        result.add(place);
+                      }
                     }
+                    cityState.value = result;
                   }
-                  cities = result;
-                }
-              }, 
-            ),
-          ),  
-          Expanded(
-            child: cities.isEmpty ? 
-              const Text("No data found :(")
-             : 
-              ListView.builder(
-                itemCount: cities.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                      leading: const Icon(Icons.location_on),
-                      title: GestureDetector(
-                        onTap: () {
-                          onPressed(cities[index], flag);
-                          Navigator.pop(context);
-                        },
-                        child: Text(cities[index])
-                      ),
-                  );
-                }
+                }, 
               ),
-          ),
-        ]
-      );
+            ),  
+            Expanded(
+              child: cities.isEmpty ? 
+                const Text("No data found :(")
+               : 
+                ListView.builder(
+                  itemCount: cities.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                        leading: const Icon(Icons.location_on),
+                        title: GestureDetector(
+                          onTap: () {
+                            onPressed(cities[index], flag);
+                            Navigator.pop(context);
+                          },
+                          child: Text(cities[index])
+                        ),
+                    );
+                  },
+                  shrinkWrap: true,
+                ),
+            ),
+          ]
+        ),
+    );
   }
 }
--- lib/screens/location.dart
+++ lib/screens/location.dart
@@ -3,6 +3,7 @@ import 'package:waliyalift/components/button.dart';
 import 'package:waliyalift/components/card.dart';
 import 'package:waliyalift/components/location_picker.dart';
 import 'package:waliyalift/components/text.dart';
+import 'package:waliyalift/models/place.dart';
 import 'package:waliyalift/utils/color.dart';
 
 class Location extends StatefulWidget {
@@ -16,129 +17,138 @@ class _LocationState extends State<Location> {
   String borderColor = "#000000";
   String pickLocation = "Pick location";
   String dropLocation = "Drop location";
+  List<Place> places = [
+     Place(id: "1", country: "Ethiopia", city: "Addis Ababa", zone: "1", street: "Asmera road", title: "KMU", lat: "12.33", lng: "34.34"),
+          Place(id: "1", country: "Ethiopia", city: "Addis Ababa", zone: "2", street: "Megenagna", title: "Metebaber blg", lat: "12.33", lng: "36.34")
+  ];
+
+  void pick(location, flag) {
+    if (flag == "pick") {
+        setState(() {
+          pickLocation = location;
+        });
+    }else {
+        setState(() {
+          dropLocation = location;
+        });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
+      appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
         title: MyText(
-          text: "Pick Location", 
-          size: 12, 
-          bgcolor: colorToHexString(Theme.of(context).colorScheme.primary), 
-          fgcolor: "#ffffff",
-          isBold: true,
-          borderRadius: 0
-        ),
+            text: "Pick Location",
+            size: 12,
+            bgcolor: colorToHexString(Theme.of(context).colorScheme.primary),
+            fgcolor: "#ffffff",
+            isBold: true,
+            borderRadius: 0),
         centerTitle: true,
       ),
       body: Center(
         child: Column(children: [
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           Image.asset(
             'assets/images/location.png',
             height: 180,
             width: 180,
           ),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           MyCard(
-          width: 250, 
-          height: 50, 
-          borderRadius: 5, 
-          onPressed: () => {
-            showDialog(
-              context: context, 
-              builder: (BuildContext context) {
-                return togglePopup("pick");
-              }
-            )
-          }, 
-          borderColor: borderColor,
-          widget: Container(
-            padding: const EdgeInsets.all(6),
-            color: getColorFromHex("#ffffff"),
-            child: Row(children: [
-               MyText(
-                text: pickLocation,
-                size: 12,
-                bgcolor: "#ffffff",
-                borderRadius: 0),
-              const SizedBox(
-                width: 115,
-              ),
-              Image.asset(
-                "assets/images/location-icon.png",
-                height: 20,
-                width: 20,
+            width: 250,
+            height: 50,
+            borderRadius: 5,
+            onPressed: () => {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => LocationPicker(
+                  places: places,
+                  flag: "pick",
+                  onPressed: (location, flag) => pick(location, "pick"),
+                ))
               )
-            ]),
+            },
+            borderColor: borderColor,
+            widget: Container(
+              padding: const EdgeInsets.all(6),
+              color: getColorFromHex("#ffffff"),
+              child: Row(children: [
+                Expanded(
+                  child: MyText(
+                      text: pickLocation,
+                      size: 12,
+                      bgcolor: "#ffffff",
+                      borderRadius: 0),
+                ),
+                const SizedBox(
+                  width: 115,
+                ),
+                Image.asset(
+                  "assets/images/location-icon.png",
+                  height: 20,
+                  width: 20,
+                )
+              ]),
+            ),
           ),
-        ),
-        const SizedBox(height: 20,),
-        MyCard(
-          width: 250, 
-          height: 50, 
-          borderRadius: 5,
-          borderColor: borderColor, 
-          onPressed: () => {
-            showDialog(
-              context: context, 
-              builder: (BuildContext context) {
-                return togglePopup("drop");
-              }
-            )
-          }, 
-          widget: Container(
-            padding: const EdgeInsets.all(6),
-            color: getColorFromHex("#ffffff"),
-            child: Row(children: [
-               MyText(
-                text: dropLocation,
-                size: 12,
-                bgcolor: "#ffffff",
-                borderRadius: 0),
-              const SizedBox(
-                width: 113,
-              ),
-              Image.asset(
-                "assets/images/location-icon.png",
-                height: 20,
-                width: 20,
+          const SizedBox(
+            height: 20,
+          ),
+          MyCard(
+            width: 250,
+            height: 50,
+            borderRadius: 5,
+            borderColor: borderColor,
+            onPressed: () => {
+             Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => LocationPicker(
+                  places: places,
+                  flag: "drop",
+                  onPressed: (location, flag) => pick(location, "drop"),
+                ))
               )
-            ]),
+            },
+            widget: Container(
+              padding: const EdgeInsets.all(6),
+              color: getColorFromHex("#ffffff"),
+              child: Row(children: [
+                Expanded(
+                  child: MyText(
+                      text: dropLocation,
+                      size: 12,
+                      bgcolor: "#ffffff",
+                      borderRadius: 0),
+                ),
+                const SizedBox(
+                  width: 113,
+                ),
+                Image.asset(
+                  "assets/images/location-icon.png",
+                  height: 20,
+                  width: 20,
+                )
+              ]),
+            ),
           ),
-        ),
-        const SizedBox(height: 20,),
-        MyButton(
-          text: "Next", 
-          width: 250, 
-          height: 40, 
-          borderRadius: 5,
-          bgcolor: "#165214",
-          onPressed: () {}
-        )
+          const SizedBox(
+            height: 20,
+          ),
+          MyButton(
+              text: "Next",
+              width: 250,
+              height: 40,
+              borderRadius: 5,
+              bgcolor: "#165214",
+              onPressed: () {})
         ]),
-      ),
-    );
-  }
-
-  Widget togglePopup(String flag) {
-    return AlertDialog(
-      title: Text("$flag location"),
-      content: LocationPicker(
-        places: [],
-        onPressed: (location, flag) => {
-          if(flag == "pick") {
-            setState((){
-              pickLocation = location;
-            })
-          }else {
-            setState((){
-              dropLocation = location;
-            })
-          }
-        },
-        flag: flag,
       ),
     );
   }
